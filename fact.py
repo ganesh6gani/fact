f63c2047c88d692c4f72745a8b27092e4e22ce84
ganesh6gani <105957560+ganesh6gani@users.noreply.github.com>
2022-09-03 19:36:53 +0530

    Add files via upload

---
+++ fact.py
@@ -0,0 +1,11 @@
+n = int(input("Enter number: "))
+fact = 1
+
+if n < 0:
+    print("neg")
+elif n == 0:
+    print("{}! = {}".format(n, fact))
+else:
+    for i in range(n, 1, -1):
+        fact = fact * i
+    print("{}! = {}".format(n, fact))
